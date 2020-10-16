773ba32f667daa80743df20f7ce42ca6e8647841
Daniel Farmer <dfarmer@creditsense.com.au>
2020-10-16 15:02:15 +1000

    updated server.js to be a bit nicer

--- server.js
+++ server.js
@@ -1,71 +1,79 @@
-'use strict';
+"use strict";
 
-const WebSocket = require('ws');
-const mqtt = require('mqtt')
-const config = require('./config.json');
-const socket = new WebSocket(`ws://${config.deconz.host}:${config.deconz.port}`)
+const WebSocket = require("ws");
+const mqtt = require("mqtt");
+const config = require("./config.json");
+const socket = new WebSocket(
+  `ws://${config.deconz.host}:${config.deconz.port}`
+);
 
 var mqttConnected = false;
 
-const client = mqtt.connect(`mqtt://${config.mqtt.host}:${config.mqtt.port}`, { 
-  keepalive: 10000, 
+const client = mqtt.connect(`mqtt://${config.mqtt.host}:${config.mqtt.port}`, {
+  keepalive: 10000,
   clientId: "deconz-to-mqtt",
-  username: config.mqtt.username, 
-  password: config.mqtt.password
+  username: config.mqtt.username,
+  password: config.mqtt.password,
 });
 
-client.on('error', () => {
-  console.log('MQTT connection failure or parsing error');
+client.on("error", () => {
+  console.log("MQTT connection failure or parsing error");
   mqttConnected = false;
 });
 
-client.on('offline', () => {
-  console.log('MQTT going offline');
+client.on("offline", () => {
+  console.log("MQTT going offline");
   mqttConnected = false;
 });
 
-client.on('connect', () => {
-  console.log('MQTT Server connected');
+client.on("connect", () => {
+  console.log("Connected to MQTT Server!!");
   mqttConnected = true;
 });
 
-client.on('end', () => {
-  console.log('MQTT shutdown');
+client.on("end", () => {
+  console.log("MQTT shutdown");
   mqttConnected = false;
 });
 
-client.on('message', (topic,message) => {
-  console.log('onMessageArrived:' + message.payloadString);
+client.on("message", (topic, message) => {
+  console.log("onMessageArrived:" + message.payloadString);
 });
 
-socket.on('open', () => {
-  socket.on('message', data => {
-    const sensorData = JSON.parse(data)
+socket.on("open", () => {
+  console.log("Connected to Phoscon!");
+  socket.on("message", (data) => {
+    const sensorData = JSON.parse(data);
+    if (parseInt(sensorData.id, 10) > 4) {
+      console.log("sensorData", sensorData);
+    }
     const sensorId = sensorData.id;
     const sensor = config.sensors[sensorId];
     if (sensor === undefined) {
-      console.log(`sensor not defined: ${sensorId}`)
-      return
+      // console.log(`sensor not defined: ${sensorId}`);
+      return;
     }
 
-    const topic = `${sensor.topic}`
-    const dataType = `${sensor.data}`
+    const topic = `${sensor.topic}`;
+    const dataType = `${sensor.data}`;
+    if (!sensorData.state || !sensorData.state[dataType]) {
+      console.log("got sensor info without state, 🤔");
+      return;
+    }
     const value = sensorData.state[dataType] / sensor.divisor;
-    
-    console.log(`topic: ${topic} data: ${value}`)
 
     if (mqttConnected) {
-      client.publish(topic, `${value}`)
-      console.log("data published");
-    }
-    else {
+      client.publish(topic, `${value}`);
+      console.log(`topic: ${topic} data: ${value} published`);
+    } else {
+      console.log(`topic: ${topic} data: ${value} not published - `);
       console.log("mqtt not connected.");
     }
   });
 });
 
-socket.on('error', () => {
-  console.log('something has gone wrong');
+socket.on("error", () => {
+  console.log("something has gone wrong");
 });
 
-console.log('started');
+console.log("started");
